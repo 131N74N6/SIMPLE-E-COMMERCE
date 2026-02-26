dd229f2f32c6d234a838471d03bf64621f7e7841
unknown <bintangkusumam@gmail.com>
2026-02-26 12:58:04 +0700

    still-fixing

--- api/src/index.ts
+++ api/src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     methods: ['DELETE', 'GET', 'POST', 'PUT'],
-    origin: ["http://localhost:5173", "http://localhost:1234", "https://api-e-shop.vercel.app"]
+    origin: ["http://localhost:5173", "http://localhost:1234", "https://api-e-shop.vercel.app/api"]
 }));
 app.use('/api/auth', authRateLimiter, authRouters);
 app.use('/api/cart', cartRouters)
