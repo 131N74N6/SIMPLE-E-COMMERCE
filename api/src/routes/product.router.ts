a00a573cc16bbdca4c47b65ccf8bd0d932629da6
unknown <bintangkusumam@gmail.com>
2026-02-26 23:08:18 +0700

    >_<

--- api/src/routes/product.router.ts
+++ api/src/routes/product.router.ts
@@ -11,7 +11,7 @@ const productRouters = Router();
 productRouters.delete('/delete-chosen', verifyToken, deleteChosenProducts);
 productRouters.delete('/delete/:_id', verifyToken, deleteOneProduct);
 productRouters.delete('/deletes/:user_id', verifyToken, checkOwnership, deleteAllProducts);
-productRouters.get('/get-all', verifyToken, getAllProducts);
+productRouters.get('/get-all', getAllProducts);
 productRouters.get('/owner/:user_id', verifyToken, getUserProducts);
 productRouters.get('/total/:user_id', verifyToken, checkOwnership, getUserTotalProducts);
 productRouters.get('/search', verifyToken, getSearchedProducts);
